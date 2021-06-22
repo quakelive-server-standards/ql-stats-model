da1bd9ecffaedb533d20ab9505be72e836f76a39
René Gastmeier <rene@coderitter.io>
2021-06-22 22:08:15 +0200

    Add additional values to ModType

--- src/types/ModType.ts
+++ src/types/ModType.ts
@@ -1,9 +1,38 @@
 export enum ModType {
+  BFG = 'BFG',
+  BFG_SPLASH = 'BFG splash damage',
+  CHAINGUN = 'Chain Gun',
+  CRUSH = 'Crush',
+  /**
+   * When the player was falling to death.
+   */
+  FALLING = 'Falling',
+  GAUNTLET = 'Gauntlet',
+  GRENADE = 'Grenade Launcher',
+  GRENADE_SPLASH = 'Grenade splash damage',
+  HMG = 'Heavy Machine Gun',
+  /**
+   * When the player falls out of the arena.
+   */
+  HURT = 'Hurt',
+  KAMIKAZE = 'Kamikaze',
   LAVA = 'Lava',
-  LIGHTNING = 'Lightning',
-  PLASMA = 'Plasma',
-  ROCKET = 'Rocket',
-  ROCKET_SPLASH = 'Rocket splash',
+  LIGHTNING = 'Lightning Gun',
+  MACHINEGUN = 'Machine Gun',
+  NAILGUN = 'Nailgun',
+  PLASMA = 'Plasma Gun',
+  PLASMA_SPLASH = 'Plasma splash damage',
+  PROXMINE = 'Proximity Mine Launcher',
+  PROXMINE_SPLASH = 'Proximity Mine splash damange',
+  RAILGUN = 'Railgun',
+  ROCKET = 'Rocket Launcher',
+  ROCKET_SPLASH = 'Rocket splash damage',
+  SHOTGUN = 'Shotgun',
   SLIME = 'Slime',
-  SWITCHTEAM = 'Switch team'
+  SWITCHTEAM = 'Switch team',
+  TRIGGER_HURT = 'Trigger hurt',
+  /**
+   * Unkwown world death
+   */
+  UNKNOWN = 'Unknown'
 }
